69d0cf389cd4273b1dd217e75ffa6b1e828a275e
Nicolasrlima <nicolasrlima@gmail.com>
2020-08-11 22:23:55 -0300

    NavigationSidebar css done, pending animations

--- umctest-nicolas-lima/src/components/NavigationSidebar.vue
+++ umctest-nicolas-lima/src/components/NavigationSidebar.vue
@@ -1,25 +1,31 @@
 <template>
   <section v-if="sidebarStatus">
+    <img id="logo" src="../assets/brand.svg" alt="PEG Contas Logo" />
     <nav>
-      <img src="../assets/brand.svg" alt="PEG Contas Logo" />
       <button @click="sidebarStatus = false">X</button>
       <a href="/minhas-atividades">
-        <img src="../assets/accounts.svg" />Minhas Atividades
+        <img src="../assets/accounts.svg" />
+        <span>Minhas Atividades</span>
       </a>
       <a href="/todas-as-contas">
-        <img src="../assets/accounts.svg" />Todas as contas
+        <img src="../assets/accounts.svg" />
+        <span>Todas as contas</span>
       </a>
       <a href="/usuarios">
-        <img src="../assets/user.svg" />Usuários
+        <img src="../assets/user.svg" />
+        <span>Usuários</span>
       </a>
       <a href="/desempenho">
-        <img src="../assets/performance.svg" />Desempenho
+        <img src="../assets/performance.svg" />
+        <span>Desempenho</span>
       </a>
       <a href="/dashboard">
-        <img src="../assets/dashboard.svg" />Dashboard
+        <img src="../assets/dashboard.svg" />
+        <span>Dashboard</span>
       </a>
     </nav>
   </section>
+  <button @click="sidebarStatus = true" v-else>Abrir</button>
 </template>
 
 <script>
@@ -34,26 +40,54 @@ export default {
 <style scoped>
 section {
   height: 100%;
-  width: 10rem;
+  width: 8rem;
   background-color: #e7ebf7;
   position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#logo {
+  width: 100%;
+}
+
+nav {
+  text-align: center;
+  width: 100%;
+}
+
+a,
+button {
+  margin-top: 1.5rem;
 }
 
 button {
   background-color: transparent;
   border-color: transparent;
   cursor: pointer;
-}
-
-nav {
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  align-items: center;
+  color: #e7ebf7;
+  background-color: #2793ff;
+  border-radius: 50%;
+  border: 2px solid #2793ff;
+  position: absolute;
+  right: -10px;
+  font-size: 1.2rem;
 }
 
 a {
+  display: flex;
+  flex-direction: column;
   color: #a1acc9;
+  align-items: center;
+}
+
+a:hover {
+  color: #2793ff;
+  border-left: 4px solid #2393ff;
+}
+
+span {
+  width: 6rem;
 }
 </style>
